bd4635e1457b15efb74c514152b47709c0772162
VILBERT Alexandre <61315791+AlexV2204@users.noreply.github.com>
2020-10-13 11:41:35 +0200

    Update wsgi.py

--- LeRelais/LeRelais/wsgi.py
+++ LeRelais/LeRelais/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeRelaisO.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeRelais.settings')
 
 application = get_wsgi_application()
