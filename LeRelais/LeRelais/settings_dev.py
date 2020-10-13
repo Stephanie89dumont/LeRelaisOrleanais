5790f44057ee798008ec16a927f07e71fa9ab2f7
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-10-13 12:07:22 +0200

    Revert "correction de bug pour permettre la mise en ligne du site"
    This reverts commit 941ccae9469b4caae04fb3366ccc9d449053b40e.

--- LeRelais/LeRelais/settings_dev.py
+++ LeRelais/LeRelais/settings_dev.py
@@ -1,9 +1,20 @@
+"""local runserver settings"""
+​
 import os
 from .settings import BASE_DIR
+​
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'y1$p&l05j8rbg%_se7^cc#5+35a!wkl()wek-x9tu*qm&9$9o3'
 
-SECRET_KEY = "y1$p&l05j8rbg%_se7^cc#5+35a!wkl()wek-x9tu*qm&9$9o3"
+​
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+​
 ALLOWED_HOSTS = []
+​
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+​
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
