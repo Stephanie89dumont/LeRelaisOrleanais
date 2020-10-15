3c7da32ddf891ee250cebf5c7abc7462215981cc
PierreRWCS <61827591+PierreRWCS@users.noreply.github.com>
2020-10-15 17:11:47 +0200

    ultime modifs
    contenues de tous ce qui touche de prêt ou de loin la page info-press

---
+++ LeRelaisOrleanais-template/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("Pres", views.Pres, name="Pres"),
+    path("Vous", views.Vous, name="Vous"),
+    path("Agir", views.Agir, name="Agir"),
+    path("Soutenir", views.Soutenir, name="Soutenir"),
+    path("Connexion", views.Connexion, name="Connexion"),
+    path("Inscription", views.Inscription, name="Inscription"),
+    path("Info-press",views.Infopress, name="Info-press"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
